5ba52bee8c0728f37812e9e2575c24f0ca7055f9
lukasbecvar <becvarlukas99@gmail.com>
2025-12-27 12:37:00 +0100

    added unauthorized test to AntilogControllerTest

--- tests/Controller/AntilogControllerTest.php
+++ tests/Controller/AntilogControllerTest.php
@@ -23,18 +23,36 @@ class AntilogControllerTest extends CustomTestCase
     protected function setUp(): void
     {
         $this->client = static::createClient();
-
-        // simulate login
-        $this->simulateLogin($this->client);
     }
 
     /**
-     * Test enable antilog
+     * Test enable antilog when user is unauthorized
      *
      * @return void
      */
-    public function testEnableAntiLog(): void
+    public function testEnableAntiLogWhenUserIsUnauthorized(): void
     {
+        // simulate login
+        $this->simulateLogin($this->client, 'User');
+
+        $this->client->request('POST', '/antilog/5369362536', [
+            'csrf_token' => $this->getCsrfToken($this->client)
+        ]);
+
+        // assert response
+        $this->assertResponseStatusCodeSame(Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * Test enable antilog when user is authorized
+     *
+     * @return void
+     */
+    public function testEnableAntiLogWhenUserIsAuthorized(): void
+    {
+        // simulate login
+        $this->simulateLogin($this->client, 'Admin');
+
         $this->client->request('POST', '/antilog/5369362536', [
             'csrf_token' => $this->getCsrfToken($this->client)
         ]);
